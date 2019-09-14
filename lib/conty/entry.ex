f0d2277edf53e48dc6124d464a38fe135a2c40a8
José Esteves <ajesteves@gmail.com>
2019-09-14 12:21:21 -0300

    add balance primitive validations needs refactor

--- lib/conty/entry.ex
+++ lib/conty/entry.ex
@@ -2,6 +2,7 @@ defmodule Conty.Entry do
   use Ecto.Schema
 
   alias Conty.{Entry, EntryItem}
+  alias Decimal, as: D
 
   schema "entries" do
     field :date, :date
@@ -19,6 +20,16 @@ defmodule Conty.Entry do
       :description
     ])
     |> Ecto.Changeset.cast_assoc(:entry_items)
+    |> validate_balance
   end
 
+  def validate_balance(changeset) do
+    Ecto.Changeset.validate_change(changeset, :entry_items, fn _, _ ->
+      items = Ecto.Changeset.apply_changes(changeset).entry_items
+      cond do
+        (Enum.map(items, &(Map.get(&1, :amount))) |> Enum.reduce(D.new(0), &D.add(&1, &2))) == D.new(0) -> []
+        true -> [error: "bad balance"]
+      end
+    end)
+  end
 end
