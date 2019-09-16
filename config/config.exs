1ac4730764d40b1974230bcc4582cd9550a40901
Jose Esteves <jose@ceibo.co>
2019-09-16 11:17:56 -0300

    use env

--- config/config.exs
+++ config/config.exs
@@ -3,9 +3,7 @@ use Mix.Config
 config :conty, Conty.Repo,
   database: "conty_dev",
   username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  # OR use a URL to connect instead
-  url: "postgres://postgres:postgres@localhost/ecto_simple"
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: "localhost"
 
 config :conty, ecto_repos: [Conty.Repo]
