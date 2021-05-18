5f23fac1153cc3f0d894f9387c5a0e05f598b0e3
Jose Esteves <jose@ceibo.co>
2021-05-18 18:35:51 -0300

    test

--- test/conty_test.exs
+++ test/conty_test.exs
@@ -1,8 +1,19 @@
 defmodule ContyTest do
   use ExUnit.Case
   doctest Conty
+  alias Conty.{Entry, EntryItem}
 
-  test "greets the world" do
-    assert Conty.hello() == :world
+  test "Entry should be balance" do
+    changeset = %Entry{}
+    |> Entry.changeset(%{
+      date: Date.utc_today(),
+      entry_items: [
+        %{amount: 100},
+        %{amount: -10}
+      ]
+    })
+
+    assert not changeset.valid?
+
   end
 end
