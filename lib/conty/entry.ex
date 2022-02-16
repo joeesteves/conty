ca7fa6770427e2abb704934c70dc70d04fb28b38
Joe Esteves <joe@caravana.dev>
2022-02-16 18:23:58 -0300

    :hammer:

--- lib/conty/entry.ex
+++ lib/conty/entry.ex
@@ -1,6 +1,9 @@
 defmodule Conty.Entry do
-  @moduledoc false
+  @moduledoc """
+    Journal Entry: it holds 2 or more items (lines) that nets to zero (balanced Journal).
+  """
   use Ecto.Schema
+
   import Ecto.Changeset
 
   alias Conty.{Entry, EntryItem}
@@ -29,7 +32,7 @@ defmodule Conty.Entry do
     validate_change(changeset, :entry_items, fn _, _ ->
       items = apply_changes(changeset).entry_items
 
-      if entry_balanced?(items), do: [], else: [base: "not balanced"]
+      if entry_balanced?(items), do: [], else: [base: "entry not balanced, entry items amount must net to zero"]
     end)
   end
 
