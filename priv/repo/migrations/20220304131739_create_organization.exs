63a60045273635e64556ef7b04929dd192c4357e
Joe Esteves <joe@caravana.dev>
2022-03-08 10:39:38 -0300

    add organizations migration

--- priv/repo/migrations/20220304131739_create_organization.exs
+++ priv/repo/migrations/20220304131739_create_organization.exs
@@ -2,6 +2,12 @@ defmodule Conty.Repo.Migrations.CreateOrganization do
   use Ecto.Migration
 
   def change do
+    create table(:organizations) do
+      add(:name, :string)
 
+      timestamps()
+    end
+
+    create(index(:organizations, [:name]))
   end
 end
